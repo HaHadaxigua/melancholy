5551b4d8f3f48bce90b4c06649c1ea5903c0499d
HaHadaxigua <Hahadaxigua@gmail.com>
2020-11-21 23:19:39 +0800

    normal: 测试

--- back/pkg/store/log.go
+++ back/pkg/store/log.go
@@ -1,6 +1,9 @@
 package store
 
-import "github.com/HaHadaxigua/melancholy/pkg/model/user"
+import (
+	"fmt"
+	"github.com/HaHadaxigua/melancholy/pkg/model/user"
+)
 
 //SaveExitLog 记录退出log
 func SaveExitLog(el *user.ExitLog) error {
@@ -13,6 +16,10 @@ func SaveExitLog(el *user.ExitLog) error {
 
 //FindExitLog 寻找退出日志
 func FindExitLog(token string) (*user.ExitLog, error) {
+
+	fmt.Println("测试")
+
+
 	db := GetConn()
 	el := &user.ExitLog{}
 	res := db.Model(el).Where("token = ?", token).Scan(el)
@@ -23,5 +30,4 @@ func FindExitLog(token string) (*user.ExitLog, error) {
 	} else {
 		return el, nil
 	}
-
 }
